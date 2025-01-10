3cea137544298df368614d198720743539d34d68
Yassir-Aamari <yassiraamari08@gmail.com>
2025-01-10 16:21:41 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,24 +2,32 @@
 
 # Function to count the number of files in the current directory
 count_files() {
-    echo $(ls -l | grep -v ^l | wc -l)
+  echo $(ls -1 | wc -l)
 }
 
-# Get the correct number of files
-correct_guess=$(count_files)
+# Main loop for the guessing game
+guessing_game() {
+  # Get the correct number of files
+  correct_number=$(count_files)
+  
+  # Initialize guess variable
+  guess=-1
 
-# Prompt user to guess
-echo "Guess how many files are in the current directory."
-
-# Loop until the user guesses correctly
-while true; do
+  # Loop until the guess is correct
+  while [ "$guess" -ne "$correct_number" ]; do
+    echo "How many files are in the current directory?"
     read guess
-    if [[ $guess -lt $correct_guess ]]; then
-        echo "Your guess is too low. Try again!"
-    elif [[ $guess -gt $correct_guess ]]; then
-        echo "Your guess is too high. Try again!"
+
+    if [ "$guess" -lt "$correct_number" ]; then
+      echo "Your guess is too low!"
+    elif [ "$guess" -gt "$correct_number" ]; then
+      echo "Your guess is too high!"
     else
-        echo "Congratulations! You guessed it correctly!"
-        break
+      echo "Congratulations! Your guess is correct."
     fi
-done
+  done
+}
+
+# Start the game
+guessing_game
+
